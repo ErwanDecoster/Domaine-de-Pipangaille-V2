9f7e490e75f8f7b59947d58210bb7152a85fe1da
ErwanDecoster <wanerd2@gmail.com>
2025-12-23 13:37:35 +0100

    feat: init astro with basic library

---
+++ astro.config.mjs
@@ -0,0 +1,20 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import vue from '@astrojs/vue';
+
+import tailwindcss from '@tailwindcss/vite';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [vue()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  i18n: {
+    locales: ["fr", "en"],
+    defaultLocale: "en",
+  }
+});
