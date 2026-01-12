0f7e0b2cfec5fd953d649e15d3cb1ebbc3d3b17b
ErwanDecoster <wanerd2@gmail.com>
2026-01-12 09:55:31 +0100

    fix: update website URL for La Table des SAJ restaurant

--- src/constants/surroundings.ts
+++ src/constants/surroundings.ts
@@ -443,7 +443,7 @@ export const surroundings = {
         },
       ],
       mapsUrl: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2808.6052215576483!2d4.782907876440744!3d45.255775247320926!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47f5150bc1dfa54b%3A0x8885f334a6ef92c7!2sLa%20Table%20des%20SAJ!5e0!3m2!1sfr!2sfr!4v1767736458071!5m2!1sfr!2sfr',
-      website: '',
+      website: 'https://la-table-des-saj-restaurant-st-desirat.eatbu.com',
       socialMedia: [
         {
           platform: 'facebook',
